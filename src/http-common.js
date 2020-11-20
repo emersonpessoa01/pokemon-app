44c4612658341ecedbd21de32ad72568079bf9dc
emersonpessoa01 <eps011108@gmail.com>
2020-11-20 11:37:57 -0300

    changed baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://localhost:8081.com/',
+  // baseURL: 'https://localhost:8081.com/',
+  baseURL: 'https://back-pokemon-api.herokuapp.com/pokemon/',
   headers: {
     'Content-type': 'application/json',
   },
